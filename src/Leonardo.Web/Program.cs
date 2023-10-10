948ba07ea01f7f092a7028bac55f987d0414b5b5
N01r4M <58330155+N01r4M@users.noreply.github.com>
2023-10-10 14:48:52 +0200

    update dotnet.yml & dockerfile

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -3,7 +3,7 @@ using Leonardo;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! 3");
+app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/Fibonacci", 
     async () => await Fibonacci.RunAsync(new []{"44", "43"}));
